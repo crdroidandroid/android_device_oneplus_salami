7f3fe653e317001e8581f4560a956d8c1be28fbb
JYRRC <javixhtsweet@gmail.com>
2025-07-11 17:36:44 +0200

    salami: added device flags
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- lineage_salami.mk
+++ lineage_salami.mk
@@ -14,6 +14,12 @@ $(call inherit-product, device/oneplus/salami/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := false
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 PRODUCT_NAME := lineage_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
